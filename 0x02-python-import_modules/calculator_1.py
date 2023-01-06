31646de909523670da3b00b1fdd181f456cc0fc0
PabloJermin <jerminamartey@gmail.com>
2023-01-06 05:41:34 -0800

    calculator function

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def add(a, b):
+    Args:
+        a: first integer
+        b: second ineger
+        return (a + b)
+    def sub(a, b):
+        Args:
+            a: first integer
+            b: second integer
+            Returns:
+                The return value. a - b
+                return (a - b)
+            def mul(a, b):
+                Args:
+                    a: first integer
+                    b: second integer
+                    return (a * b)
+                def div(a, b):
+                    Args:
+                        a: first integer
+                        b: second intege
+                        return int(a / b)
